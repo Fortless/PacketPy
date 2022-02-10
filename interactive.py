e56eaae91bc4672d0575446db96fe02e887bc23d
Fortless <64139203+Fortless@users.noreply.github.com>
2022-02-10 21:50:37 +0200

    make people who run the code without reading it undersgand

--- interactive.py
+++ interactive.py
@@ -1,2 +1,5 @@
 # Just an idea, didn't have time to do de code yet 😂
 # This will be an interface to manage the payloads with numbers etc
+print("Read the file contents")
+print("Exiting...")
+exit()
